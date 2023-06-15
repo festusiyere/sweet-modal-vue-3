9358b86404d1fed01fe6ea91744f17760a090417
Festus Iyere <ifeanyifestus@gmail.com>
2023-06-15 14:33:02 +0100

    Added Tree Shaking Support

--- src/sweet-modal-vue-3.ts
+++ src/sweet-modal-vue-3.ts
@@ -1,8 +1,12 @@
 import type { App } from 'vue'
 import SweetModal from './components/SweetModal.vue'
+import SweetModalTab from './components/SweetModalTab.vue'
 
 export default {
     install(app: App) {
         app.component('SweetModal', SweetModal)
+        app.component('SweetModalTab', SweetModalTab)
     }
 }
+
+export { SweetModal, SweetModalTab }
